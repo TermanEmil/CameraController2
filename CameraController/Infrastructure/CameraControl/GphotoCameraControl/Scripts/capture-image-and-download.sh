9ee03099acd22cd1c21b4ce7f0ad468eda498427
TermanEmil <terman.emil@gmail.com>
2021-02-07 23:18:21 +0100

    Created the script for gphoto2 image capturing

---
+++ CameraController/Infrastructure/CameraControl/GphotoCameraControl/Scripts/capture-image-and-download.sh
@@ -0,0 +1,26 @@
+﻿#!/bin/bash
+
+# Parameters
+#	$1: filename
+#	$2: port
+
+# Example output from gphoto2 command given filename "Foo":
+#	New file is in location /capt0000.nef on the camera
+#	Saving file as Foo.nef
+#	Deleting file /capt0000.nef on the camera
+
+# Expected output from script
+#	Foo.nef
+
+# gphoto2:
+#	--filename %C: file sufix
+
+# sed:
+#	-n: no print (skip likes that don't match')
+#	p: print what matched
+#	I: case insensitive
+
+# Capture image on the given port and download it with the given name.
+# Filter out only the final filename (whatever follows 'Saving file as ')
+gphoto2 --capture-image-and-download --filename "$1.%C" --port "$2" --force-overwrite\
+|	sed -n 's/Saving file as //Ip'
