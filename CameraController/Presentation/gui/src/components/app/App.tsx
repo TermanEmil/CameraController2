4fd2e12d3b919f65e5a17035f847c49c604b4227
TermanEmil <terman.emil@gmail.com>
2021-05-12 00:46:23 +0200

    List cameras

--- CameraController/Presentation/gui/src/components/app/App.tsx
+++ CameraController/Presentation/gui/src/components/app/App.tsx
@@ -1,5 +1,9 @@
 import './App.css';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import CameraIcon from '@material-ui/icons/Camera';
 import { ErrorHandler, useErrorHandler } from 'components/common/error-handler/ErrorHandler';
 import LoadingFab from 'components/common/loading-fab/LoadingFab';
@@ -7,6 +11,15 @@ import { useState } from 'react';
 
 import { CameraModel } from './CameraModel';
 import getCameras from './GetCameras';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      backgroundColor: theme.palette.background.paper,
+    },
+  }),
+);
 
 export default function App(): JSX.Element {
   function DummyInnerComponent() {
@@ -19,17 +32,23 @@ export default function App(): JSX.Element {
         .catch((error) => handleError(`Failed to autodetect: ${error.message}`));
     }
 
+    const classes = useStyles();
+
     return (
       <>
         <LoadingFab onClick={handleAutoDetect}>
           <CameraIcon />
         </LoadingFab>
 
-        {cameras.map((camera, i) => (
-          <div key={i}>
-            {camera.model} | {camera.port}
-          </div>
-        ))}
+        <div className={classes.root}>
+          <List component="nav" aria-label="main mailbox folders">
+            {cameras.map((camera, i) => (
+              <ListItem key={i} button>
+                <ListItemText primary={camera.model} secondary={camera.port} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
       </>
     );
   }
