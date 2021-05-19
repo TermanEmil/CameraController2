43b4278569277270e49b358a4115c1fa7f0569ce
TermanEmil <terman.emil@gmail.com>
2021-05-20 00:46:19 +0200

    Testing: mocked validation

--- CameraController/Presentation/gui/src/components/app/getCameras.test.ts
+++ CameraController/Presentation/gui/src/components/app/getCameras.test.ts
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
+import { getCamerasResponseSchema } from './CameraModel';
 import getCameras from "./getCameras";
 
 jest.mock('axios');
 jest.mock('./urls');
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
 
 it('throws on API error', async () => {
   mockedAxios.get.mockRejectedValue('Some error');
@@ -17,13 +22,18 @@ it('returns no cameras on empty response', async () => {
   await expect(getCameras()).resolves.toStrictEqual([]);
 });
 
-// TODO: try to mock the validation
 it("validates the API's response", async () => {
   mockedAxios.get.mockResolvedValue({data: [{foo: 'bar'}]});
+  jest.spyOn(getCamerasResponseSchema, 'validate').mockRejectedValue('error');
   await expect(getCameras()).rejects.toThrow();
 });
 
 it('returns cameras from the API', async () => {
-  mockedAxios.get.mockResolvedValue({data: [{model: 'foo', port: 'bar'}]});
-  await expect(getCameras()).resolves.toHaveLength(1);
+  mockedAxios.get.mockResolvedValue({data: [{model: 'foo'}]});
+  jest.spyOn(getCamerasResponseSchema, 'validate').mockReturnValue(Promise.resolve({ cameras: [] }));
+
+  const cameras = await getCameras();
+
+  expect(cameras).toHaveLength(1);
+  expect(cameras[0].model).toBe('foo');
 });
