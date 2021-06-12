6f07fcee8d7b031e3bf093edf4bbc3a7bfb7d74f
TermanEmil <terman.emil@gmail.com>
2021-06-13 00:33:35 +0200

    Routing

--- CameraController/Presentation/gui/src/components/app/App.tsx
+++ CameraController/Presentation/gui/src/components/app/App.tsx
@@ -1,22 +1,21 @@
 import './App.css';
 
 import { ErrorHandler } from 'components/common/error-handler/ErrorHandler';
-import { createBrowserHistory } from 'history';
-import { Route, Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import CameraList from './CameraList';
 import CameraView from './CameraView';
 
 export default function App(): JSX.Element {
-  const history = createBrowserHistory();
-
   return (
     <div>
       <ErrorHandler>
-        <Router history={history}>
-          <Route exact path="/" component={CameraList} />
-          <Route path="/cameras/:port" component={CameraView} />
-        </Router>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={CameraList} />
+            <Route path="/cameras/:port" component={CameraView} />
+          </Switch>
+        </BrowserRouter>
       </ErrorHandler>
     </div>
   );
